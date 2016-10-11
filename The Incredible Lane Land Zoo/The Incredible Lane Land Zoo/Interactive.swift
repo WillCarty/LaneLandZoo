8607cb17970d60c06a8e7174e97a9ce0fbe0d1f1
William Carty <will.carty@interapthq.com>
2016-10-11 19:39:53 -0400

    Project Beginning

---
+++ The Incredible Lane Land Zoo/The Incredible Lane Land Zoo/Interactive.swift
@@ -0,0 +1,19 @@
+
+
+import Foundation
+class Interactive{
+    private var done: Bool = false;
+    private var currentInput:String = "q";
+    private var io = IO()
+    var totalBill: Double = 0.0
+    var partyTotal:Int = 0
+    var tipPercent:Double = 0.0
+    var subTotal:Double = 0.0
+    var totalPerPerson:Double = 0.0
+    
+    func go() {
+        
+        while !done{
+
+
+}
---
+++ The Incredible Lane Land Zoo/The Incredible Lane Land Zoo/PeopleClass.swift
@@ -0,0 +1,9 @@
+//
+//  PeopleClass.swift
+//  The Incredible Lane Land Zoo
+//
+//  Created by Will Carty on 10/11/16.
+//  Copyright © 2016 Will Carty. All rights reserved.
+//
+
+import Foundation
